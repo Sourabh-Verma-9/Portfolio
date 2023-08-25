866ea128674581622d00e86420df3bc1b08a871f
Sourabh Verma <sourabhv961681@gmail.com>
2023-08-26 00:01:42 +0530

    Finished Portfolio site

---
+++ script.js
@@ -0,0 +1,6 @@
+toggleMenu=()=>{
+    const menu=document.querySelector(".mobile-nav-links");
+    const icon=document.querySelector(".mobile-nav-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
